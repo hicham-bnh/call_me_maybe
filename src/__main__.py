164b7bd8c8fdf8bb29cf6c993a4f5a35e3117f06
Mohammed Hicham Benhabib <mobenhab@z3r2p2.42lyon.fr>
2026-03-06 10:39:12 +0100

    begin of parsing the files

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,48 @@
 import sys
+import argparse
 from src.default import result_input
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "--functions_definition",
+            default="data/input/functions_definition.json"
+            )
+        parser.add_argument(
+            "--input",
+            default="data/input/function_calling_tests.json"
+        )
         result_input()
+        parser.add_argument(
+            "--output",
+            default="data/output/functions_calls.json"
+        )
+        args = parser.parse_args()
+        print("Functions:", args.functions_definition)
+        print("Input:", args.input)
+        print("Output:", args.output)
+        with open(args.functions_definition, "r") as fd:
+            test = fd.read()
+            print(test)
     else:
-        print("fichier")
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "--functions_definition",
+            default="data/input/functions_definition.jason"
+            )
+        parser.add_argument(
+            "--input",
+            default="data/input/function_calling_tests.jason"
+        )
+        result_input()
+        parser.add_argument(
+            "--output",
+            default="data/output/functions_calls.jason"
+        )
+        args = parser.parse_args()
+        print("Functions:", args.functions_definition)
+        print("Input:", args.input)
+        print("Output:", args.output)
+        
