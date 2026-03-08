7a5ce93c0d189bb0bbb42ec4e34aeb342437cb3f
Mohammed Hicham Benhabib <mobenhab@z3r6p5.42lyon.fr>
2026-03-08 15:43:54 +0100

    desciption des fonctions fini

--- src/parsing.py
+++ src/parsing.py
@@ -1,14 +1,18 @@
 from src.default import files
 import argparse
-from typing import List, Dict
+from typing import List, Dict, Any
 import json
 from pydantic import BaseModel
 
 
 class Parsing:
-    intput_function: str
-    intput_call: str 
-    output_file: str
+    def __init__(self) -> None:
+        self.intput_function: str
+        self.intput_call: str 
+        self.output_file: str
+        self.data_call: Any
+        self.data_func: Any
+
     def pars_files(self):
         file_lunch = files()
         parser = argparse.ArgumentParser()
@@ -30,7 +34,16 @@ class Parsing:
         self.output_file = args.output
         file_lunch.lunch(self.output_file)
         with open(self.intput_call, "r") as f:
-            data = json.load(f)
+            self.data_call = json.load(f)
         with open(self.intput_function, "r") as f:
-            func = json.load(f)
-        return data, func
+            self.data_func = json.load(f)
+        self.function_format()
+
+    def function_format(self):
+        result = []
+        for f in self.data_func:
+            name = f['name']
+            dscp = f['description']
+            params = ", ".join(f['parameters'].keys())
+            result.append(f"-{name}({params}): {dscp}")
+        print(result)
