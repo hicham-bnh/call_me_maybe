232033fe20f0cf5c13d080f50b97a8496cf7f3ee
hicham-bnh <benhabibmohammedhicham@gmail.com>
2026-03-13 11:22:34 +0100

    vocabulary management done

--- src/__main__.py
+++ src/__main__.py
@@ -3,7 +3,10 @@ from src.default import Files
 from src.result import Llm
 
 if __name__ == "__main__":
-        llm = Llm()
-        pars = Parsing()
-        call, func = pars.pars_files()
-        llm.get_vocab()
+        try:
+                llm = Llm()
+                pars = Parsing()
+                call, func = pars.pars_files()
+                llm.get_vocab()
+        except Exception as e:
+                print(e)
--- src/result.py
+++ src/result.py
@@ -1,13 +1,17 @@
 from llm_sdk import Small_LLM_Model
 import json
+from typing import Dict
 
 class Llm:
     def __init__(self) -> None:
         self.model = Small_LLM_Model()
-        
+        self.token_to_id: Dict[str, int]
+        self.id_to_token: Dict[int, str]
+
+
     def get_vocab(self):
         vocab_path = self.model.get_path_to_vocab_file()
         with open(vocab_path) as fd:
-            data = json.load(fd)
-        print(vocab_path)
-        print(data)
+            self.token_to_id = json.load(fd)
+        self.id_to_token = {int(v): k for k, v in self.token_to_id.items()}
+        print(self.id_to_token)
