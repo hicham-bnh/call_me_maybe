e086cc88244d9c5882f30781e8c8f4d25a7620e2
hicham-bnh <benhabibmohammedhicham@gmail.com>
2026-03-14 00:50:20 +0100

    path vocabulary manage

--- src/result.py
+++ src/result.py
@@ -6,12 +6,15 @@ class Llm:
     def __init__(self) -> None:
         self.model = Small_LLM_Model()
         self.vocab: Dict = {}
+        self.token_to_id: Dict = {}
+        self.id_to_token: Dict = {}
 
 
     def get_vocab(self):
         vocab_path = self.model.get_path_to_vocab_file()
         with open(vocab_path, encoding='utf-8') as fd:
             self.vocab = json.load(fd)
-
+        self.token_to_id = {v: k for k, v in self.vocab.items()}
+        self.id_to_token = {k: v for k, v in self.vocab.items()}
 
     
