2dd938cc9741a09b5ca2255bf98c577755d7152b
hicham-bnh <benhabibmohammedhicham@gmail.com>
2026-03-13 13:24:57 +0100

    to finish after

--- src/result.py
+++ src/result.py
@@ -5,13 +5,12 @@ from typing import Dict
 class Llm:
     def __init__(self) -> None:
         self.model = Small_LLM_Model()
-        self.token_to_id: Dict[str, int]
-        self.id_to_token: Dict[int, str]
+        self.vocab: Dict
 
 
     def get_vocab(self):
         vocab_path = self.model.get_path_to_vocab_file()
-        with open(vocab_path) as fd:
-            self.token_to_id = json.load(fd)
-        self.id_to_token = {int(v): k for k, v in self.token_to_id.items()}
-        print(self.id_to_token)
+        with open(vocab_path, encoding='utf-8') as fd:
+            self.vocab = json.load(fd)
+        print(self.vocab)
+        
