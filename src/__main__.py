155017ec233c243883068b49a72ab68145ef7297
hicham-bnh <benhabibmohammedhicham@gmail.com>
2026-04-26 07:11:29 +0200

    parsing and open json file added and managed

--- src/__main__.py
+++ src/__main__.py
@@ -3,10 +3,14 @@ from .parsing import Pars
 
 
 if __name__ == "__main__":
-    test = Pars()
-    if (len(sys.argv) == 1):
-        test.default_pars()
-    elif (len(sys.argv) == 7):
-        test.pars_file(sys.argv)
-    elif (len(sys.argv) != 4):
-        print("error")
+    try:
+        test = Pars()
+        if (len(sys.argv) == 1):
+            test.default_pars()
+        elif (len(sys.argv) == 7):
+            test.pars_file(sys.argv)
+        elif (len(sys.argv) != 4):
+            print("error")
+        test.open_files()
+    except Exception as e:
+        print(e)
--- src/parsing.py
+++ src/parsing.py
@@ -1,31 +1,41 @@
 import argparse
 from pydantic import BaseModel
-from typing import List
+from typing import Any, List
 from pathlib import Path
+import json
 import os
 
-class Pars(BaseModel):
+class Pars:
+	def __init__(self) -> None:
+		self.parser = argparse.ArgumentParser()
+		self.args: Any = None
+		
 	def default_pars(self) -> None:
-		parser = argparse.ArgumentParser()
-		parser.add_argument('--functions_definition',
+		self.parser.add_argument('--functions_definition',
 			default='data/input/functions_definition.json')
-		parser.add_argument('--input',
+		self.parser.add_argument('--input',
 			default='data/input/function_calling_tests.json')
-		parser.add_argument('--output',
+		self.parser.add_argument('--output',
 			default='data/output/function_calls.json')
-		args = parser.parse_args()
-		output = Path(args.output)
+		self.args = self.parser.parse_args()
+		output = Path(self.args.output)
 		output.parent.mkdir(parents=True, exist_ok=True)
 
 
 	def pars_file(self, files: List[str]) -> None:
-		parser = argparse.ArgumentParser()
-		parser.add_argument('--functions_definition',
+		self.parser.add_argument('--functions_definition',
 			default=files[1])
-		parser.add_argument('--input',
+		self.parser.add_argument('--input',
 			default=files[2])
-		parser.add_argument('--output',
+		self.parser.add_argument('--output',
 			default=files[3])
-		args = parser.parse_args()
-		output = Path(args.output)
+		self.args = self.parser.parse_args()
+		output = Path(self.args.output)
 		output.parent.mkdir(parents=True, exist_ok=True)
+
+	
+	def open_files(self) -> None:
+		with open(self.args.input, "r", encoding='utf-8') as fd:
+			promps = json.load(fd)
+		for k in promps:
+			print(k['prompt'])
