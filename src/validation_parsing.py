23f44fea21c45cb6341bd49f78868ee6a7206765
hicham-bnh <benhabibmohammedhicham@gmail.com>
2026-04-28 00:23:15 +0200

    parsing and validation data updated

--- src/validation_parsing.py
+++ src/validation_parsing.py
@@ -1,14 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import Dict
 
 
 class Calling(BaseModel):
+	model_config=ConfigDict(extra="forbid")
 	prompt: str
 
 class Type(BaseModel):
+	model_config=ConfigDict(extra="forbid")
 	type: str
 
 class Definition(BaseModel):
+	model_config=ConfigDict(extra="forbid")
 	name: str
 	description: str
 	parameters: Dict[str, Type]
